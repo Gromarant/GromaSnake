544b0fb1c4459aebe4e94e64f49177c261d9ab13
Mariangelica Rodriguez <gromarant@gmail.com>
2022-11-07 19:30:31 +0100

    Set random colors for the food

--- components/food.js
+++ components/food.js
@@ -1,7 +1,9 @@
 "use strict";
 
-import { onSnake, expands } from './snake.js';
+import { onSnake, expands, addOnePart } from './snake.js';
 import { randomPosition } from '../grid.js';
+
+const growing = 1;
 
 const getRandomPosition = () => {
     let newFoodPosition;
@@ -12,19 +14,23 @@ const getRandomPosition = () => {
 };
 
 let foodBody = getRandomPosition();
-const growing = 1;
+const randomColor = () => '#'+ Math.floor( Math.random() * 16777215 ).toString( 16 );
+let setColor = randomColor();
 
 export const update = () => {
     if( onSnake( foodBody ) ) {
         expands( growing );
         foodBody = getRandomPosition();
-    }
+        setColor = randomColor();
+    };
 };
 
 export const render = ( board ) => {
-      const food = document.createElement('div');
+      const food = document.createElement('span');
+      food.id = 'randomFood';
       food.style.gridRowStart = foodBody.y;
       food.style.gridColumnStart = foodBody.x;
       food.classList.add( 'randomFood' );
+      food.style.backgroundColor = setColor;
       board.appendChild( food );
 };
